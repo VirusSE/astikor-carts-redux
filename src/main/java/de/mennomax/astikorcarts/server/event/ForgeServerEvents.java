7a770197d69fdaa0013321893c4e236bdebc9b58
jusipat <justin10patrick@gmail.com>
2024-07-28 09:15:53 -0600

    Abstracted lightning config options for carts

--- src/main/java/de/mennomax/astikorcarts/server/event/ForgeServerEvents.java
+++ src/main/java/de/mennomax/astikorcarts/server/event/ForgeServerEvents.java
@@ -2,7 +2,7 @@ package de.mennomax.astikorcarts.server.event;
 
 import de.mennomax.astikorcarts.AstikorCarts;
 import de.mennomax.astikorcarts.config.AstikorCartsConfig;
-import de.mennomax.astikorcarts.entity.SupplyCartEntity;
+import de.mennomax.astikorcarts.entity.AbstractDrawnEntity;
 import net.minecraftforge.event.entity.EntityStruckByLightningEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -11,7 +11,7 @@ import net.minecraftforge.fml.common.Mod;
 public class ForgeServerEvents {
     @SubscribeEvent
     public static void entityStruckByLightningEvent(EntityStruckByLightningEvent event) {
-            if (event.getEntity() instanceof SupplyCartEntity cart && AstikorCartsConfig.get().lightningInvulnerable.get()) {
+            if (event.getEntity() instanceof AbstractDrawnEntity && AstikorCartsConfig.get().lightningInvulnerable.get()) {
                 event.setCanceled(true);
         }
     }
