e8a3f3a4e56196b47c9b55f69fd3ee4ef76724e4
VirusSE <virusse@virusse.net>
2025-01-02 10:29:55 +0100

    Migrated ForgeServerEvents.java to Neoforge and renamed the file.

--- src/main/java/de/mennomax/astikorcarts/server/event/ServerEvents.java
+++ src/main/java/de/mennomax/astikorcarts/server/event/ServerEvents.java
@@ -3,12 +3,13 @@ package de.mennomax.astikorcarts.server.event;
 import de.mennomax.astikorcarts.AstikorCarts;
 import de.mennomax.astikorcarts.config.AstikorCartsConfig;
 import de.mennomax.astikorcarts.entity.AbstractDrawnEntity;
-import net.minecraftforge.event.entity.EntityStruckByLightningEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
+import net.neoforged.neoforge.event.entity.EntityStruckByLightningEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
 
-@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE, modid = AstikorCarts.ID)
-public class ForgeServerEvents {
+@Mod(AstikorCarts.ID)
+public class ServerEvents {
+
     @SubscribeEvent
     public static void entityStruckByLightningEvent(EntityStruckByLightningEvent event) {
         if (event.getEntity() instanceof AbstractDrawnEntity && AstikorCartsConfig.get().lightningInvulnerable.get()) {
